d0b2ff941f9727c6c7740548a8d0dc393fcb60ee
leeing <372711472@qq.com>
2018-10-21 22:31:21 +0800

    加入vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,12 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <Test v-bind:msg="msg" v-bind:content="ptc" v-on:changeWithParent="change" />
-    <span>父页面：{{msg}}</span>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
   </div>
 </template>
-
-<script>
-import Test from './components/test/index';
-
-export default {
-  name: 'app',
-  data () {
-    return {
-      msg: '来自父页面',
-      ptc: [
-        {
-          content: '我是来自父页面的数据1',
-          id: 1
-        },
-        {
-          content: '我是来自父页面的数据2',
-          id: 2
-        },
-        {
-          content: '我是来自父页面的数据3',
-          id: 3
-        }
-      ]
-    };
-  },
-  components: {
-    Test
-  },
-  methods: {
-    change(val) {
-      this.msg = val;
-    }
-  }
-}
-</script>
 
 <style>
 #app {
---
+++ src/views/Home.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <Test v-bind:msg="msg" v-bind:content="ptc" v-on:changeWithParent="changeWithParent"/>
+    <span>{{msg}}</span>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Test from '../components/test'
+
+export default {
+  name: 'home',
+  data() {
+    return {
+      msg: '来自父页面',
+      ptc: [
+        {
+          content: '列表1',
+          id: 1
+        },
+        {
+          content: '列表2',
+          id: 2
+        },
+        {
+          content: '列表3',
+          id: 3
+        },
+      ]
+    }
+  },
+  methods: {
+    changeWithParent(val) {
+      this.msg = val;
+    }
+  },
+  components: {
+    Test
+  }
+}
+</script>
