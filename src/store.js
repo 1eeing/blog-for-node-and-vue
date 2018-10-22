fe3000912743deb8ddd1753437a46442c955e910
leeing <372711472@qq.com>
2018-10-23 01:08:24 +0800

    学习并实操Vuex

---
+++ src/store.js
@@ -0,0 +1,25 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    count: 0
+  },
+  mutations: {
+    increment(state) {
+      state.count++;
+    },
+    decrement(state) {
+      state.count--;
+    }
+  },
+  actions: {
+    decrement({commit}) {
+      setTimeout(() => {
+        commit('decrement');
+      }, 1000);
+    }
+  }
+})
---
+++ src/views/Count.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <button v-on:click="increment">+</button>
+        <span>{{count}}</span>
+        <button v-on:click="decrement">-</button>
+    </div>
+</template>
+
+<script>
+import {mapActions} from 'vuex';
+export default {
+    name: 'count',
+    computed: {
+        count() {
+            return this.$store.state.count;
+        }
+    },
+    methods: {
+        increment() {
+            this.$store.commit('increment');
+        },
+        ...mapActions([
+            'decrement'
+        ])
+    }
+}
+</script>
